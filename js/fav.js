538d1f43e12a6ef6c530cf8c78d6940defdc8d82
FarahJamal <farahsarese@gmail.com>
2021-06-16 20:31:47 +0300

    more css

--- js/fav.js
+++ js/fav.js
@@ -50,7 +50,12 @@ let city8 = new Cities ('Tahiti', 'https://i2.wp.com/thegoodlifefrance.com/wp-co
    getChart();
 
 
-   
+   /* <div class="container">
+        <img src="../images/card2.jpg" alt="Avatar" class="image" style="width:100%">
+        <div class="middle">
+          <div class="text">John Doe</div>
+        </div>
+      </div>*/
    function showing (){
 
        
@@ -59,34 +64,46 @@ let city8 = new Cities ('Tahiti', 'https://i2.wp.com/thegoodlifefrance.com/wp-co
 
            let city1 = document.createElement('img');
            let text1 = document.createElement('h6');
-          //  let btn = document.createElement('button');
-           let a = document.createElement('a');
-           let link = document.createTextNode("Book Now !");
-           let bdiv = document.createElement('div');
-
-
-
-           img1.appendChild(city1);
+           let bookBtn=document.createElement('button');
+         //  let a = document.createElement('a');
+          // let link = document.createTextNode("Book Now !");
+        /*   let bdiv = document.createElement('div');*/
+          /* img1.appendChild(city1);
            img1.appendChild(text1);
            img1.appendChild(bdiv);
-           bdiv.appendChild(a);
-           a.appendChild(link); 
-
-          //  btn.appendChild(a);
-          //  btn.appendChild(a);
-
+           bdiv.appendChild(a);*/
+           //a.appendChild(link); 
            city1.setAttribute('src', allCities[i].source);
            text1.textContent = (allCities[i].name);
+           //bookBtn.textContent='BookNow';
            
-          //  btn.textContent = ('Book Now!');
-           a.href= "http://127.0.0.1:5501/pages/blog.html"
+           //a.href= "http://127.0.0.1:5501/pages/blog.html"
           
+           let container=document.createElement('div');
+           container.setAttribute('id','container3');
+           city1.setAttribute('id','image');
+           container.appendChild(city1);
+           let middle=document.createElement('div');
+           middle.setAttribute('id','middle');
+           let text=document.createElement('div');
+           text.setAttribute('id','text');
+           text.appendChild(text1);
+           text.appendChild(bookBtn);
+           bookBtn.addEventListener('click',visitPage);
+
+           middle.appendChild(text);
+           container.appendChild(middle);
+           img1.appendChild(container);
+           
 
        }
      
 
    }
 
+   function visitPage(){
+    window.location='../pages/booking.html';
+}
   
   
 
@@ -95,18 +112,18 @@ let city8 = new Cities ('Tahiti', 'https://i2.wp.com/thegoodlifefrance.com/wp-co
    console.log(arrOffav);
 function getChart (){
     var barColors = [
-        "#b91d47",
-        "#00aba9",
-        "#2b5797",
-        "#e8c3b9",
-        "#1e7145",
-        "#2b5797",
-        "#e8c3b9",
-        "#1e7145"
+        "#642D06",
+        "#A64B0A",
+        "#853C09",
+        "#C67234",
+        "#EA853E",
+        "#E1DBC3",
+        "#ECB390",
+        "#851800"
 
       ];
 let ctx = document.getElementById('charts');
-
+ctx.height=500;
 new Chart("charts", {
     type: "doughnut",
     data: {
@@ -124,3 +141,4 @@ new Chart("charts", {
     }
   });
 }
+
